548319356d44df51d899e776dd37ff27da40b466
Alexander Sharko <krivodubag@gmail.com>
2021-06-09 09:34:37 +0300

    Initial Commit

---
+++ FLY Concierge/FLY_ConciergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FLY_ConciergeApp.swift
+//  FLY Concierge
+//
+//  Created by Alexander Sharko on 09.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct FLY_ConciergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
